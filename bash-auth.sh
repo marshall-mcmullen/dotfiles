6983ab5627ae672c3aa5b63fcc96d6b49e51afa1
Marshall McMullen <marshall.mcmullen@gmail.com>
2024-08-26 13:05:47 -0600

    Make >> bold in bash-auth.

--- bash-auth.sh
+++ bash-auth.sh
@@ -12,7 +12,7 @@ if [[ ! -v SSH_AGENT_PID ]] || ! kill -0 "${SSH_AGENT_PID}" &>/dev/null; then
   echo "export SSH_AUTH_SOCK=${SSH_AUTH_SOCK}" >  ~/.ssh/agent.env
   echo "export SSH_AGENT_PID=${SSH_AGENT_PID}" >> ~/.ssh/agent.env
 else
-    echo -e " ${COLOR_GREEN}>>${COLOR_NONE} Found existing ssh-agent: ${COLOR_CYAN}${SSH_AGENT_PID}${COLOR_NONE}"
+    echo -e "${COLOR_BOLD}${COLOR_GREEN}>>${COLOR_NONE} Found existing ssh-agent: ${COLOR_CYAN}${SSH_AGENT_PID}${COLOR_NONE}"
 fi
 
 # Add all private keys
@@ -22,10 +22,10 @@ for key in ~/.ssh/id_*; do
     fi
 
     if ! ssh-add -l | grep -q "$(ssh-keygen -lf "${key}" | awk '{print $2}')"; then
-        echo -e " ${COLOR_GREEN}>>${COLOR_NONE} Adding SSH key: ${COLOR_CYAN}${key}${COLOR_NONE}"
+        echo -e "${COLOR_BOLD}${COLOR_GREEN}>>${COLOR_NONE} Adding SSH key: ${COLOR_CYAN}${key}${COLOR_NONE}"
         ssh-add "${key}"
     else
-        echo -e " ${COLOR_GREEN}>>${COLOR_NONE} Known ssh key: ${COLOR_CYAN}${key}${COLOR_NONE}"
+        echo -e "${COLOR_BOLD}${COLOR_GREEN}>>${COLOR_NONE} Known ssh key: ${COLOR_CYAN}${key}${COLOR_NONE}"
     fi
 done
 
